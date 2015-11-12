2c991e84b7d68a631ab5e01ecfec661ad51a0234
Dario Martini <dario@it-consultis.net>
2015-11-12 14:48:19 +0800

    moved file to correct location

--- lib/user_issues_hook_listener.rb
+++ lib/user_issues_hook_listener.rb
@@ -2,7 +2,7 @@ class TestHookListener < Redmine::Hook::ViewListener
   
   def view_account_left_bottom(context)
     # find all tickets assigned to the user
-    context[:issues] = Issue.visible.open.find_all_by_assigned_to_id(context[:user][:id])
+    context[:issues] = Issue.visible.open.where(assigned_to_id: context[:user][:id])
     context[:controller].send(:render_to_string, {
       :partial =>'user/issues_assigned',
       :locals => context
